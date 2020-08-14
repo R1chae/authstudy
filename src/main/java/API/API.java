74cbccf0575f72899e16a81ae0e6f9f5aa09249e
Richard Dumser <richard.dumser@specific-group.com>
2020-08-14 13:39:42 +0200

    added database functionality using local javaDB (Derby) database. Added login function (/UserAuthentication).

--- src/main/java/API/API.java
+++ src/main/java/API/API.java
@@ -5,6 +5,11 @@
  */
 package API;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,17 +34,36 @@ public class API {
     @PostMapping("/RegisterNewUser")
     @ResponseStatus(HttpStatus.CREATED)
     public String postUserInfoController(String email, String username, String password, int age) {
-        currentUser = Core.User.currentUser(username, password, email, age);
-        StringBuilder registrationmsg = new StringBuilder(email);               //I could just return it as json, but for learning purposes I'll leave it with this string using StringBuilder
-        registrationmsg.append(" registered as ").append(username);
-        registrationmsg.append(" with password ").append(password);
-        registrationmsg.append(" aged ").append(age);
-        return registrationmsg.toString();
+        
+        try{
+            Infrastructure.Infrastructure.register(username, password, email, age);
+            
+            currentUser = Core.User.currentUser(username, password, email, age);
+            
+            StringBuilder registrationmsg = new StringBuilder(email);           //I could just return it as json, but for learning purposes I'll leave it with this string using StringBuilder
+            registrationmsg.append(" registered as ").append(username);
+            registrationmsg.append(" with password ").append(password);
+            registrationmsg.append(" aged ").append(age);
+            return registrationmsg.toString();
+        } catch(SQLException e){
+            e.printStackTrace();
+            return("Database call failed");
+        }
     }
 
     @PostMapping("/UserAuthentication")
     @ResponseStatus(HttpStatus.CREATED)
     public String postLoginController(String username, String password) {
-        return "not implemented yet";
+        try{
+            currentUser = Infrastructure.Infrastructure.login(username, password);//using the currentUser for this means that if someone is logged in already, they get logged out if they enter invalid credentials, BUT I don't have to make another User object
+            if(currentUser == null){
+                return "invalid credentials";
+            } else {
+                return "logged in as: " + currentUser.getUsername();            //should I use StringBuilder even for this?
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+            return "SQL ERROR";
+        }
     }
 }
--- src/main/java/Infrastructure/Infrastructure.java
+++ src/main/java/Infrastructure/Infrastructure.java
@@ -5,11 +5,49 @@
  */
 package Infrastructure;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 /**
  *
  * @author RichardDumser
  */
 public class Infrastructure {
+    private static final String DBPATH = "jdbc:derby:src\\main\\java\\Infrastructure\\authstudyDB;create=true";
+    private static final String DBUSERNAME = "root";
+    private static final String DBPASSWORD = "";
+    private static Connection con;
     
+    public static void connect() throws SQLException{
+        if(con == null){
+            con = DriverManager.getConnection(DBPATH, DBUSERNAME, DBPASSWORD);
+        }
+    }
+    
+    public static void register(String username, String password, String email, int age) throws SQLException{
+        connect();
+        Statement statement = con.createStatement();                            //TABLE user: (username varchar(64), password varchar(128), email varchar(128), age int, PRIMARY KEY(username)
+        StringBuilder sb = new StringBuilder("INSERT INTO users VALUES ('");
+        sb.append(username).append("', '");
+        sb.append(password).append("', '");
+        sb.append(email).append("', ");
+        sb.append(age).append(")");
+        statement.executeUpdate(sb.toString());
+    }
+    
+    public static Core.User login(String username, String password) throws SQLException{
+        connect();
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM users");
+        while (rs.next()) {
+            if((rs.getString("username").equals(username)) && (rs.getString("password").equals(password))){
+                return Core.User.currentUser(rs.getString("username"), rs.getString("password"), rs.getString("email"), rs.getInt("age"));
+            }
+        }
+        return null;
+    }
 }
